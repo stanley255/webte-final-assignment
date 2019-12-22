a5226668bce85ffdf78f285b24ba94ecf5e3b8ee
Matej Friedel <xfriedel@stuba.sk>
2019-12-22 18:15:28 +0100

    Pridanie funkcie pohybu a rotacie aut

--- js/classes/car.js
+++ js/classes/car.js
@@ -9,6 +9,11 @@ class Car {
         this.carOff = document.getElementById(this.color + BLINKER_OFF);
         this.carRight = document.getElementById(this.color + BLINKER_RIGHT);
         this.carLeft = document.getElementById(this.color + BLINKER_LEFT);
+        this.transformX = 0;
+        this.transformY = 0;
+        this.angle = 0;
+        this.layer = document.getElementById("layer1");
+        this.layer.style.transformOrigin = "42px 84px";
 
         this.carOff.addEventListener('click', function(){
             JUNCTION.addCarToSolution(CARS[color]);
@@ -22,7 +27,7 @@ class Car {
         this.carLeft.style.visibility = left;
     }
 
-    turnOffBliners() {
+    turnOffBlinkers() {
         this.setVisibilityLayerStates("visible", "hidden", "hidden");
     }
 
@@ -40,6 +45,32 @@ class Car {
 
     showCar() {
         this.setVisibilityLayerStates("visible", "hidden", "hidden");
+    }
+    
+    moveRelative(a, b) {
+        this.transformX += a;
+        this.transformY += b;
+        this.updateCar();
+    }
+
+    moveAbsolute(x, y) {
+        this.transformX = x;
+        this.transformY = y;
+        this.updateCar();
+    }
+    
+    rotateRelative(angle) {
+        this.angle += angle;
+        this.updateCar();
+    }
+
+    rotateAbsolute(angle) {
+        this.angle = angle;
+        this.updateCar();
+    }
+
+    updateCar() {
+        $(this.layer).attr("transform", "translate(" + this.transformX + "," + this.transformY + ") rotate(" + this.angle + ")");
     }
 
 }
