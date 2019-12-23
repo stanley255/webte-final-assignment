e7628531e7bac04be33c146838f061dbfcdfe058
adik6555 <adik6555@gmail.com>
2019-12-23 15:18:38 +0100

    changed styling of namesday widget, removed unnecessary functions, next step will use jquery grep for namesday search

--- js/namesday.js
+++ js/namesday.js
@@ -72,51 +72,6 @@ function getToday() {
     return (month.toString()+day.toString());
 }
 
-// 1. We check what field was updated last, whether it was date or textinput(name).
-// Based on this info we lookup the other field, e.g.: if date was entered last, we
-// suppose the user wants to find out the names day on that date and if name was entered
-// last, we suppose the user wants to see when that name celebrates.
-
-function namesDayLookup() {
-    if (lastUpdated=="date-input"){
-        let date = parseDateInput();
-        let names = retrieveNameFromDate(date);
-        printName(names,date);
-    }
-    else if(lastUpdated=="name-input"){
-        let name = $("#name-input").val();
-        let dates = retrieveDateFromName(name);
-        printDate(name,dates);
-    }
-} 
-
-function printName(names, date){
-    if (names.length==0){
-        console.log("Pre zadaný dátum nebolo v kalendári nájdene meno");
-        return;
-    }
-    let output = "V deň "+formatDate(date)+" majú sviatok tieto mená: ";
-    for (let i=0;i<names.length;++i){
-        console.log(names[i].nationality);
-        output+=" "+names[i].nationality+": ";
-        output+=names[i].names.textContent+";";
-    }
-    console.log(output);
-}
-
-function printDate(name,dates){
-    if (dates.length==0){
-        console.log("Pre vstup "+name+" nebol nájdený záznam v kalendári");
-        return;
-    }
-    let output = "Meno "+name.toString()+" má sviatok v tieto dni: ";
-    for (let i=0;i<dates.length;++i){
-        output+=dates[i].name.nodeName+": ";
-        output+=(formatDate(dates[i].date));
-        output+=" ("+dates[i].name.textContent+") ";
-    }
-    console.log(output);
-}
 
 function formatDate(date){
     let month = date[0].toString()+date[1].toString();
@@ -145,46 +100,14 @@ function retrieveDateFromName(input){
     return dates;
 }
 
-function checkifNameEquals(input1,input2){
-    input1 = normalizeName(input1);
-    input2 = normalizeName(input2);
-    
-    // There might be multiple names on this date that have namesday
-    // so we separate them to array and check each
-    if (input2.includes(",")){
-        let inputAr = input2.split(",");
-        if (inputAr.includes(input1)){            
-            return true;
-        }
-    }
-    else{
-        if(input1==input2){
-            return true;
-        }
-    }
-    return false;
+
+function normalize(input){
+    input = input.toLowerCase();
+    input = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    input = input.replace(" ","");
+    return input;
 }
 
-function normalizeName(name){
-    name = name.toLowerCase();
-    name = name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    name = name.replace(" ","");
-    return name;
-}
-
-// function parseDateInput(){
-//     // Create and adjust day output
-//     let day = $('#select-day').val()
-//     if(day.length==1) 
-//         day = '0' + day;
-//     // Create and adjust month output
-//     let month = $('#select-month').val();
-//     if(month.length==1) 
-//         month = '0' + month;
-    
-//     let output = month + day;
-//     return output;
-// }
 
 function parseDateInput(){
     //thanks to trimDateInput function, the date will always be in format dd.dd or dd.d, so we
@@ -204,60 +127,8 @@ function parseDateInput(){
     return output;
 }
 
-$(function trimDateInput(){
-    $("#date-input").keyup(function(event) {
-        var value = $("#date-input").val();
-        //check number of chars before dot, after dot ctrl+v and max month and day
-        //if key is backspace, dont do any special action
-        if(event.keyCode==8){
-            return;
-        }
-        //if value is length two, one of which is number and one of which is dot
-        //prepend 0
-        else if(value.length==2 && value.includes('.')){
-            $("#date-input").val('0'+value);
-            console.log('case3');
-        }
-        //if key is dot and there is no dot yet, insert
-        else if(event.key=='.' && (value.match(/\./g) || []).length==1){
-            console.log('case0')
-            return;
-        }
-        //if length of date is greater than 2 and it does not include dot or is greater than 5,
-        //dont allow to insert anything more
-        else if(value.length>2 && !value.includes('.') || value.length>5){
-            $("#date-input").val(value.replace(event.key,''));
-            console.log('case1');
-        }
-        //if length of input is 2 and first two values are digits, append dot
-        else if (value.length==2 && !isNaN(parseInt(value[0])) && !isNaN(parseInt(value[1]))){
-            $("#date-input").val(value+'.');
-            console.log('case2');
-        }
-        //if there's 1 dot, don't allow to insert any more
-        else if((value.match(/\./g) || []).length>1){
-            $("#date-input").val(value.replace(event.key,''));
-            console.log('case4');
-        }
-        //if you try to insert anything other than digit, don't allow
-        else if(isNaN(parseInt(event.key))){
-            $("#date-input").val(value.replace(event.key,''));
-            console.log('case5');
-        }
-    });
+$("#namesday-input").on("keyup",function(){
+    var value = $(this.val()).toLowerCase();
+    
 });
 
-function switchLastUpdated(){
-    lastUpdated = this.id;
-}
-
-// If this function is called it means that toggle was switched, therefore we can assume
-// that we should change the input type
-function changeInputType() {
-    $('.input-box').each(function() {
-        $(this).toggle();
-    })
-    switchLastUpdated();
-    let htmlText = $("#input-toggle").is(":checked") ? "Vyhľadaj meno" : "Vyhľadaj dátum";
-    $("#search-button").html(htmlText);
-}
