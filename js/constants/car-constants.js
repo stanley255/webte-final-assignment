c10c183bbc8513fb6d8a0e90eea2491699a883af
Stanislav Pekarovič <spekarovic@gmail.com>
2019-12-27 11:21:28 +0100

    Refactor konštánt do separátnych súborov

---
+++ js/constants/car-constants.js
@@ -0,0 +1,36 @@
+const BLINKER_OFF = "-car-blinker-off";
+const BLINKER_RIGHT = "-car-blinker-right";
+const BLINKER_LEFT = "-car-blinker-left";
+
+const LAYERS = {
+    blue: "layer1",
+    green: "layer2",
+    red: "layer3"
+}
+
+const BLINKERS = {
+    off: function (car) {
+        car.turnOffBlinkers()
+    },
+    right: function (car) {
+        car.turnOnRightBlinker()
+    },
+    left: function (car) {
+        car.turnOnLeftBlinker()
+    }
+}
+
+const CAR_BLINKER_STATES = [
+    "carOff",
+    "carRight",
+    "carLeft"
+]
+
+const DEFAULT_TRANSFORM_ORIGIN = "42px 84px";
+const DEFAULT_POSITION = {x: 0, y: 0};
+const DEFAULT_ANGLE = 0;
+
+const DEFAULT_BLINKER_STATE = "off";
+
+const CAR_HALF_WIDTH = 40;
+
---
+++ js/constants/junction-controller-constants.js
@@ -0,0 +1,27 @@
+const JUNCTION_CLASS_ACTIVE = "active teal";
+const JUNCTION_CLASS_INACTIVE = "waves-effect";
+const JUNCTION_IMAGES_PATH = "./img/junctions/";
+const JUNCTION_IMAGE_EXTENSION = ".png";
+
+const CAR_SVG_PATH = "./img/cars/";
+const CAR_SVG_PREFIX = "cars-";
+const CAR_SVG_EXTENSION = ".svg";
+
+const JUNCTION_OBJECTS_PATH = "/resources/junctions/";
+const JUNCTION_OBJECTS_EXTENSION = ".json";
+
+const STARTING_JUNCTION_NUMBER = "01";
+
+const DEMO_BUTTON_ID = "#junction-demo-button";
+const DEMO_BUTTON_ACTIVE = "waves-effect waves-light btn";
+const DEMO_BUTTON_INACTIVE = "waves-effect waves-light btn disabled";
+
+const SOLUTION_BUTTON_ID = "#junction-solution-button";
+const SOLUTION_BUTTON_ACTIVE = "waves-effect waves-light btn modal-trigger";
+const SOLUTION_BUTTON_INACTIVE = "waves-effect waves-light btn modal-trigger disabled";
+
+const CORRECT_ANSWER_POP_UP_ID = "#correct-answer";
+const INCORRECT_ANSWER_POP_UP_ID = "#incorrect-answer";
+
+const JUNCTION_SOLUTION_TITLE_ID = "#junction-solution-title"
+const JUNCTION_SOLUTION_TEXT_ID = "#junction-solution-text"
