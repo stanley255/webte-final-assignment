baba70372797e0d0ab22a8265920969958f30f55
Stanislav Pekarovič <spekarovic@gmail.com>
2020-01-04 17:42:34 +0100

    Naplnenie sekcie autorov

--- js/init.js
+++ js/init.js
@@ -3,3 +3,10 @@
         $('.sidenav').sidenav();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// Disable all task-checkboxes click
+$(".task-checkbox").each((i, checkbox) => {
+    $(checkbox).on("click", function (e) {
+        e.preventDefault();
+    });
+});
